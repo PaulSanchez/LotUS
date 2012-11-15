439e1d28d7522e79a95859b8cacee0f31c85c774
pjs <pjs@alum.mit.edu>
2012-11-15 12:38:43 -0800

    Added fair & biased die to both tester implementations
    Signed-off-by: pjs <pjs@alum.mit.edu>

--- tester.py
+++ tester.py
@@ -19,6 +19,10 @@ def main():
   test_distribution([1, 2, 3, 4], [0.41, 0.3, 0.2, 0.1])
   test_distribution([1, 2, 3, 4, 5], [0.4, 0.3, 0.2, 0.1])
   test_distribution([1, 2, 3, 4], [1.4, 0.3, 0.2, 0.1])  
+  p_die = 1.0 / 6.0
+  test_distribution([1, 2, 3, 4, 5, 6], [p_die] * 6)
+  test_distribution([1, 3, 4, 5, 6],
+    [p_die, p_die, p_die, 2.0 * p_die, p_die])
   return 0
     
 if __name__ == '__main__':
