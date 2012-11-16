e02347617cfef9eacc2e4cfd67fadcad1b24b4bf
pjs <pjs@alum.mit.edu>
2012-11-16 13:47:49 -0800

    refactored distribution.rb to cache mean and variance values
    Signed-off-by: pjs <pjs@alum.mit.edu>

--- distribution.rb
+++ distribution.rb
@@ -8,6 +8,10 @@ class Distribution
   attr_reader :x
   # The array of probabilities - attr_reader gives a getter but not a setter
   attr_reader :p
+  # Cached value for mean of the distribution
+  attr_reader :mean
+  # Cached value for variance of the distribution
+  attr_reader :variance
 
   # Construct a distribution object with the specified vectors of x
   # and p_values.  Both arrays must be the same length, the probabilities
@@ -16,32 +20,29 @@ class Distribution
   def initialize(x, p_values)
     raise "Array lengths must be the same" if x.length != p_values.length
     total_prob = 0.0
-    p_values.each_index do |i|
-      raise "p_values must be between 0.0 and 1.0" \
-     	 if p_values[i] < 0.0 || p_values[i] > 1.0 
-      total_prob += p_values[i]
+    p_values.each do |p|
+      raise "p_values must be between 0.0 and 1.0" if p < 0.0 || p > 1.0 
+      total_prob += p
     end
     raise "p-values don't sum to one." if (1.0 - total_prob).abs > 1E-12
     # freezing makes the object immutable, i.e., you can't alter @x and
     # @p after they've been validated, despite having access to their
     # references via attr_reader.
-    (@x = x).freeze
-    (@p = p_values).freeze
+    (@x = x.clone).freeze
+    (@p = p_values.clone).freeze
+    @mean = E()
+    square = lambda {|value| value * value}
+    @variance = E(square) - square[@mean]
   end
 
-  # Implements expectation calculation of a function g(X) for
-  # this distribution.  If no function g is supplied, it defaults
-  # to X, i.e., it calculates the mean of the distribution.
+  # Implements "Law of the Unconcious Statistician" expectation
+  # calculation of a function g(X) for the distribution.  If no
+  # function g is supplied, it defaults to X, i.e., it calculates
+  # E[X], the mean of the distribution.
   def E(g = lambda {|x| x})   # default function is x itself, yielding E[X]
     result = 0.0
     x.each_index {|i| result += g[@x[i]] * @p[i]}
-    return result
-  end
-
-  # Determine the variance of the distribtion.
-  def variance
-    square = lambda {|value| value * value}
-    E(square) - square[E()]
+    result
   end
 
 end
